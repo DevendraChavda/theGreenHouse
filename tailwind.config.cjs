1a52958c8389838f328043b05d46d7436018af1c
Devendra <chavdadevendra03@gmail.com>
2024-08-31 01:16:52 +0530

    style update

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,20 +1,35 @@
 /** @type {import('tailwindcss').Config} */
 const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
       fontFamily: {
         sans: [
-          "Bricolage Grotesque Variable",
-          "Inter Variable",
-          "Inter",
+          "Montserrat",
+          "Raleway",
+          "Quicksand",
           ...defaultTheme.fontFamily.sans,
+        ],
+        serif: [
+          "Playfair Display",
+          "Bodoni",
+          "Crimson Pro",
+          ...defaultTheme.fontFamily.serif,
+        ],
+        mono: [
+          "Roboto Mono",
+          "Inconsolata",
+          "Cousine",
+          "monospace",
+          ...defaultTheme.fontFamily.mono,
         ],
       },
     },
   },
-  plugins: [require("@tailwindcss/typography"),
-    require('flowbite/plugin')               
+  plugins: [
+    require("@tailwindcss/typography"),
+    require('flowbite/plugin'),
   ],
 };
