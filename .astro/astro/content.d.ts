3ff6ad1a7933f2c884e4b8642b89d0ef0172fc8f
Darshil Kanzariya <darshilkanzariya8251@gmail.com>
2024-08-30 22:09:31 +0530

    complete image and form part

--- .astro/astro/content.d.ts
+++ .astro/astro/content.d.ts
@@ -205,5 +205,5 @@ declare module 'astro:content' {
 
 	type AnyEntryMap = ContentEntryMap & DataEntryMap;
 
-	export type ContentConfig = typeof import("./../../src/content/config.js");
+	export type ContentConfig = typeof import("../../src/content/config.js");
 }
